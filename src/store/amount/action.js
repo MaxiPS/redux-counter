c9054de9ac7971b9b10ac39b5ed51bff029806ff
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-03-16 10:11:48 +0000

    Feature: se crea nueva accion depositar

---
+++ src/store/amount/action.js
@@ -0,0 +1,19 @@
+// DEFINO UNA CONSTANTE PARA ESTABLECER EL TYPE DEL ACTION Y EVITAR ERRORES EN EL STRING, O SI EN ALGUN MOMENTO QUIERO CAMBIARLO
+const DEPOSITAR_DINERO = "@DEPOSITAR_DINERO";
+
+// DEFINO ACCION DEPOSITAR_DINERO
+
+
+/* Las acciones en redux sirven para informar a nuestro reducer los cambios que se van a efectuar en la APP,
+   Por Ejemplo en la accion que se muestra a continuacion, vamos a decirle que es de tipo DEPOSITAR y
+   El "Payload" o la "Carga de datos" en este caso es un entero y es 10.
+*/
+
+const depositar = () => {
+  return {
+    type: DEPOSITAR_DINERO,
+    payload: 10,
+  };
+};
+
+export {depositar}
