64721bbfe194f855f7d0ef3bcfb6c02eb96840d2
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-03-16 11:46:39 +0000

    Feature: definicion de accion retirar

--- src/store/amount/action.js
+++ src/store/amount/action.js
@@ -1,8 +1,8 @@
 // DEFINO UNA CONSTANTE PARA ESTABLECER EL TYPE DEL ACTION Y EVITAR ERRORES EN EL STRING, O SI EN ALGUN MOMENTO QUIERO CAMBIARLO
 export const DEPOSITAR_DINERO = "@DEPOSITAR_DINERO";
+export const RETIRAR_DINERO = "@RETIRAR_DINERO";
 
 // DEFINO ACCION DEPOSITAR_DINERO
-
 
 /* Las acciones en redux sirven para informar a nuestro reducer los cambios que se van a efectuar en la APP,
    Por Ejemplo en la accion que se muestra a continuacion, vamos a decirle que es de tipo DEPOSITAR y
@@ -16,4 +16,11 @@ const depositar = () => {
   };
 };
 
-export {depositar}
+const retirar = () => {
+  return {
+    type: RETIRAR_DINERO,
+    payload: 10,
+  };
+};
+
+export { depositar, retirar };
