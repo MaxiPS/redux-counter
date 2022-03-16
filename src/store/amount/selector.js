dd50ce87b01bd0af8261bb8365d4d7e31ee63edc
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-03-16 11:25:04 +0000

    Feature: definicion del selector

---
+++ src/store/amount/selector.js
@@ -0,0 +1,13 @@
+/* 
+   useSelector es un hook propio de redux, su funcionalidad es:
+   obtener el estado actual del store desde cualquier parte de la app.
+
+   Al tener nuestra app centralizada en un único store, podemos "atacar" los reducers,
+   para obtener el estado deseado, esto se hace mediamente funciones.
+
+   EN ESTE CASO , para evitar confusiones separé las funciones selectoras del reducer en este archivo.
+   Generalmente se suelen encontrar en el mismo reducer.js
+*/
+
+// El nombre del reducer es el definido en el STORE!, en este caso amountReducer.
+export const getAmount = (state) => state.amountReducer.amount;
