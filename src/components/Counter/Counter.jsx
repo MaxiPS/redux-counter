17a0ce0e4250e3edf7065c18717493472b687d5f
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-03-16 11:43:16 +0000

    Feature: implementacion de funcion depositar en el componente Counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,6 +3,9 @@ import React from "react";
 // IMPORT FUNCION SELECTORA
 import { getAmount } from "../../store/amount/selector";
 
+// IMPORT FUNCION DE ACCION
+import { depositar } from "../../store/amount/action";
+
 // IMPORT HOOKS DE REDUX
 
 import { useSelector, useDispatch } from "react-redux";
@@ -10,10 +13,14 @@ import { useSelector, useDispatch } from "react-redux";
 export const Counter = () => {
   //Utilizo el hook y hago la llamada a la funcion selectora para obtener el estado
   const amount = useSelector(getAmount);
+  // creo una constante con el hook dispatch
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>CANTIDAD : {amount}</h1>
-      <button>DEPOSITAR $10</button>
+      {/* HAGO EL DISPATCH de la action al hacer click en el boton */}
+      <button onClick={() => dispatch(depositar())}>DEPOSITAR $10</button>
       <button>RETIRAR $10</button>
     </div>
   );
