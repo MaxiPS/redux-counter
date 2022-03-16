99e8f13fc7e963a3ba04ea581527e305490b0bfb
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-03-16 11:35:11 +0000

    Feature: implementacion del selector en el componente Counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+// IMPORT FUNCION SELECTORA
+import { getAmount } from "../../store/amount/selector";
+
+// IMPORT HOOKS DE REDUX
+
+import { useSelector, useDispatch } from "react-redux";
 
 export const Counter = () => {
-  
+  //Utilizo el hook y hago la llamada a la funcion selectora para obtener el estado
+  const amount = useSelector(getAmount);
   return (
     <div>
-      <h1>CANTIDAD : 0</h1>
+      <h1>CANTIDAD : {amount}</h1>
       <button>DEPOSITAR $10</button>
       <button>RETIRAR $10</button>
     </div>
