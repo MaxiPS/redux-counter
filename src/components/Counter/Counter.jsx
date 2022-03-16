3b8b4664ca8e12fd5102a411f01be1698a2fc8fc
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-03-16 09:51:57 +0000

    Feature: Se Crea Nuevo Componente Counter

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export const Counter = () => {
+  
+  return (
+    <div>
+      <h1>CANTIDAD : 0</h1>
+      <button>DEPOSITAR $10</button>
+      <button>RETIRAR $10</button>
+    </div>
+  );
+};
+
+export default Counter;
